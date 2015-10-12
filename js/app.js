e2248ed173f8547d919724c66cba8197c244226d
adaau <ada.au.cheng@gmail.com>
2015-10-12 17:36:27 +0800

    updated .gitignore

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
+var game;
 $(document).ready(function() {
-var game = new Game();
+game = new Game();
 
 
+// game.rollDice();
+
+//game.changeDiceImage(game.dice1);
+//game.changeDiceImage(game.dice2);
 
 
+$("#roll-btn").on("click", function() {
+// Iterate through active dice: roll and change dice image
 
+  game.rollDice();
+
+  game.changeDiceImage(game.dice1);
+});
 
 
 
--- js/game.js
+++ js/game.js
@@ -2,11 +2,14 @@
 'use strict';
 
  var Game = function() {
-  this.dice1 = 6;
-  this.dice2 = 6;
-  this.dice3 = 6;
-  this.dice4 = 6;
-  this.dice5 = 6;
+  this.dice1;
+  this.dice2;
+  this.dice3;
+  this.dice4;
+  this.dice5;
+
+  this.activeDice = [];
+  this.keptDice = [];
 
   this.TWOPAIRPTS = 5;
   this.THREEOFAKINDPTS = 10;
@@ -15,6 +18,14 @@
   this.FULLHOUSEPTS = 30;
   this.FOUROFAKINDPTS = 40;
   this.LARGESTRAIGHTPTS = 50;
+
+  this.p1TwoPairs;
+  this.p1ThreeOfAKind;
+  this.p1SmallStraight;
+  this.p1Flush;
+  this.p1FullHouse;
+  this.p1FourOfAKind;
+  this.p1LargeStraight;
  }
 
 // Function to roll dice
@@ -24,4 +35,40 @@ Game.prototype.rollDice = function() {
   this.dice3 = Math.floor((Math.random() * 6)+1);
   this.dice4 = Math.floor((Math.random() * 6)+1);
   this.dice5 = Math.floor((Math.random() * 6)+1);
+}
+
+// Function to push dice element into array of active dice
+Game.prototype.setActive = function(elem) {
+  this.activeDice.push(elem);
+}
+
+// Function to push dice element into array of kept dice
+Game.prototype.setKept = function(elem) {
+  this.keptDice.push(elem);
+}
+
+// Function to change dice image (pass through game.dice1 or game.dice2 or ..., etc)
+Game.prototype.changeDiceImage = function(elem) {
+  switch(elem) {
+    case 1:
+      $("#dice1Image").attr("src", "images/1dice.png");
+      break;
+    case 2:
+      $("#dice1Image").attr("src", "images/2dice.png");
+      break;
+    case 3:
+      $("#dice1Image").attr("src", "images/3dice.png");
+      break;
+    case 4:
+      $("#dice1Image").attr("src", "images/4dice.png");
+      break;
+    case 5:
+      $("#dice1Image").attr("src", "images/5dice.png");
+      break;
+    case 6:
+      $("#dice1Image").attr("src", "images/6dice.png");
+      break;
+    default:
+      console.log("CHECK")
+  }
 }
