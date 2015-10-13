3065a6090c982401ceb051c8ad81f0da354dee65
adaau <ada.au.cheng@gmail.com>
2015-10-13 15:12:53 +0800

    added functions to roll dice, keep dice, unkeep dice

--- js/app.js
+++ js/app.js
@@ -3,22 +3,27 @@ $(document).ready(function() {
 
   game = new Game();
 
+  // when the Roll button is clicked
   $("#roll-btn").on("click", function() {
-    // change dice image based on dice value
-    $(".dice-active img").each(function(){
-      var roll = game.rollDice();
-      $(this).attr("src", "images/" + roll + "dice.png");
-      // how and where do I record the roll, i.e. the dice value?
-    })
-    // decrement rollsRemain; **** NOTE: cannot be less than 0
-    game.rollsRemain--;
-    console.log(game.rollsRemain);
+    // as long as there is at least one roll remaining
+    if(game.rollsRemain > 0) {
+      // change dice image based on dice value
+      $(".dice-active img").each(function(){
+        var roll = game.rollDice();
+        $(this).attr("src", "images/" + roll + "dice.png");
+        // how do I record the roll, i.e. the value of each dice so I can compare later? It is in var roll.
+      })
+      // decrement rollsRemain
+      game.rollsRemain--;
+      // change the corresponding rolls remaining in html
+      $("#rolls-remain").html(game.rollsRemain);
+    }
   });
 
   $(".dice").on("click", function(event) {
     // the img changes opacity
     if ($(this).css("opacity") === "1") {
-      $(this).css("opacity", "0.4");
+      $(this).css("opacity", "0.5");
       // set $(this) as keptDice by pushing to array or changing class?
     } else {
       $(this).css("opacity", "1");
@@ -29,9 +34,3 @@ $(document).ready(function() {
 
 // the div is pushed into the keptDice array
 // Function to change player turn
-// Function to show how many rolls are remaining
-/*
-set rolls as 3
-when button is clicked, rolls --
-
-*/
--- js/game.js
+++ js/game.js
@@ -1,60 +1,92 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
 'use strict';
 
- var Game = function() {
-// not sure I completely get this...
-  this.dices      = [1,2,3,4,5];
-  this.activeDice = [1,2,3,4,5];
+var Game = function() {
+  this.activeDice = [0,0,0,0,0];
   this.keptDice   = [];
-
-  this.MAXROLLS = 3;
-  this.rollsRemain = this.MAXROLLS;
-
-  this.POINTS = {
-    "TWOPAIRPTS"       : 5,
-    "THREEOFAKINDPTS"  : 10,
-    "SMALLSTRAIGHTPTS" : 20,
-    "FLUSHPTS"         : 25,
-    "FULLHOUSEPTS"     : 30,
-    "FOUROFAKINDPTS"   : 40,
-    "LARGESTRAIGHTPTS" : 50
-  };
-
-  this.p1 = {
-    "TwoPairs"      : 0,
-    "ThreeOfAKind"  : 0,
-    "SmallStraight" : 0,
-    "Flush"         : 0,
-    "FullHouse"     : 0,
-    "FourOfAKind"   : 0,
-    "LargeStraight" : 0
-  };
-
-  this.p2 = {
-    "TwoPairs"      : 0,
-    "ThreeOfAKind"  : 0,
-    "SmallStraight" : 0,
-    "Flush"         : 0,
-    "FullHouse"     : 0,
-    "FourOfAKind"   : 0,
-    "LargeStraight" : 0
-  };
 }
 
-// Function to roll dice
-Game.prototype.rollDice = function() {
-  return Math.floor((Math.random() * 6)+1);
+// Function to roll all the active dices
+Game.prototype.roll = function() {
+  this.activeDice = this.activeDice.map(function(){
+    return Math.floor((Math.random() * 6)+1);
+  });
+};
+
+// game.keepDice(keepVal) <-- dice value 1-6
+Game.prototype.keepDice = function(diceVal) {
+  var pos = this.activeDice.indexOf(diceVal);
+  if (pos != -1) {
+    this.keptDice.push(diceVal);
+    this.activeDice.splice(pos,1);
+  }
+};
+
+Game.prototype.unkeepDice = function(diceVal) {
+  var pos = this.keptDice.indexOf(diceVal);
+  if (pos != -1) {
+    this.activeDice.push(diceVal);
+    this.keptDice.splice(pos,1);
+  }
+};
+
+Game.prototype.printBoard = function() {
+  console.log("ACTIVE: " + this.activeDice);
+  console.log(" KEPT:  " + this.keptDice);
 }
 
-// Function to push dice element into array of active dice
-Game.prototype.setActive = function(elem) {
-  this.activeDice.push(elem);
-}
+var game = new Game();
+console.log("### GAME STARTED");
+game.roll();
+game.printBoard();
+console.log("### KEEP FIRST ACTIVE DICE");
+game.keepDice(game.activeDice[0]);
+game.printBoard();
+console.log("### ROLL")
+game.roll();
+game.printBoard();
+console.log("### KEEP FIRST ACTIVE DICE");
+game.keepDice(game.activeDice[0]);
+game.printBoard();
+console.log("### UNKEEP FIRST KEPT DICE");
+game.unkeepDice(game.keptDice[0]);
+game.printBoard();
+console.log("### ROLL")
+game.roll();
+game.printBoard();
+console.log("** KEEP FIRST ACTIVE DICE");
+game.keepDice(game.activeDice[0]);
+game.printBoard();
 
-// Function to push dice element into array of kept dice
-Game.prototype.setKept = function(elem) {
-  this.keptDice.push(elem);
-}
+  // this.MAXROLLS = 3;
+  // this.rollsRemain = this.MAXROLLS;
 
+  // this.POINTS = {
+  //   "TWOPAIRPTS"       : 5,
+  //   "THREEOFAKINDPTS"  : 10,
+  //   "SMALLSTRAIGHTPTS" : 20,
+  //   "FLUSHPTS"         : 25,
+  //   "FULLHOUSEPTS"     : 30,
+  //   "FOUROFAKINDPTS"   : 40,
+  //   "LARGESTRAIGHTPTS" : 50
+  // };
 
+  // this.p1 = {
+  //   "TwoPairs"      : 0,
+  //   "ThreeOfAKind"  : 0,
+  //   "SmallStraight" : 0,
+  //   "Flush"         : 0,
+  //   "FullHouse"     : 0,
+  //   "FourOfAKind"   : 0,
+  //   "LargeStraight" : 0
+  // };
 
+  // this.p2 = {
+  //   "TwoPairs"      : 0,
+  //   "ThreeOfAKind"  : 0,
+  //   "SmallStraight" : 0,
+  //   "Flush"         : 0,
+  //   "FullHouse"     : 0,
+  //   "FourOfAKind"   : 0,
+  //   "LargeStraight" : 0
+  // };
