a94d35baf1e4a046f725567087671d8b48cea22b
adaau <ada.au.cheng@gmail.com>
2015-10-13 22:32:27 +0800

    added properties to Player object

--- js/player.js
+++ js/player.js
@@ -1,3 +1,11 @@
 var Player = function() {
   this.score = 0;
+  this.twoPair = 0;
+  this.three = 0;
+  this.small = 0;
+  this.flush = 0;
+  this.full = 0;
+  this.four = 0;
+  this.large = 0;
 };
+
